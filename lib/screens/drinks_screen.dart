1b762d0e0ec2d1ed1e56102f23059cf2be7df81f
Gaby Nieva <gaby.nieva@outlook.com>
2023-07-12 16:00:35 -0600

    Initial style of cards

--- lib/screens/drinks_screen.dart
+++ lib/screens/drinks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DrinksScreen extends StatelessWidget {
   const DrinksScreen({Key? key}) : super(key: key);
@@ -6,12 +7,26 @@ class DrinksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+        toolbarHeight: 16.0,
+        //centerTitle: false,
+        leadingWidth: 100,
+        leading: Text(
+          'Cancel',
+          style: Theme.of(context).textTheme.bodySmall,
+        ),
+        actions: [
+          hour(context),
+        ],
+      ),
         body: GridView.count(
           crossAxisCount: 2,
           children: 
           [
-            oneCard(context),
-            oneCard(context),
+            SizedBox( child: oneCard(context)),
+            SizedBox( child: oneCard(context)),
+            SizedBox( child: oneCard(context)),
+            SizedBox( child: oneCard(context)),
           ],
           // List.generate(6, (index) {
           //   return Center(
@@ -26,19 +41,50 @@ class DrinksScreen extends StatelessWidget {
       );
   }
 
+   Widget hour(BuildContext context){
+    //DateTime now = DateTime.now();
+    String formattedTime = DateFormat.Hm().format(DateTime.now());
+    return Text(
+      formattedTime,
+      style: Theme.of(context).textTheme.bodySmall,
+    );
+    
+  }
+
   Widget oneCard(BuildContext context){
     return GestureDetector(
            onTap: () {
             Navigator.pop(context);
            },
-           child: const SizedBox(
-             height: 100,
-             child: Card(
-               elevation: 10,
-               color: Colors.cyanAccent,
-               child: Text('This is clickable card, tap me'),
+           child: //SizedBox(
+             //width: 50.0,
+            // height: 500.0,
+             //child: 
+             Card(
+               elevation: 10.0,
+               color: const Color.fromARGB(255, 22, 45, 67),
+               shape: RoundedRectangleBorder(
+                  side: const BorderSide(
+                    color: Color.fromARGB(255, 22, 45, 67),
+                  ),
+                  borderRadius: BorderRadius.circular(20.0), 
+               ),
+               child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '250 ml',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                    Text(
+                      'Un vaso de agua',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+               ),
              ),
-           ),
+          // ),
     );
   }
 }
